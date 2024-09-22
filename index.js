fd42d05080360bc6426a5902829cbda15e877fc6
Ariana <152944814+aam214@users.noreply.github.com>
2024-09-22 10:47:31 -0500

    Changed data-ceil and h2 to doubles Added ES6 to setTimeout. Chnaged background color and border-left. Passed countNow as an argument.

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-const countNow = document.querySelectorAll(".counter");
+const countsNow = document.querySelectorAll(".counter");
+
+countsNow.forEach(countNow => {
+  countNow.innerText = "0";
+increaseNumber(countNow);
+
+function increaseNumber(countNow) {
+let currentNumber = +countNow.innerText;
+const dataCeil = countNow.getAttribute("data-ceil");
+const increaseIt = dataCeil / 100;
+currentNumber = Math.ceil(currentNumber + increaseIt);
+
+
+if(currentNumber < dataCeil) {
+  countNow.innerText = currentNumber;
+  setTimeout(() => increaseNumber(countNow), 5);
+} else {
+countNow.innerText = dataCeil;
+}
+}
+});
+
