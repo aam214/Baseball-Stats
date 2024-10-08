992ace4c6ea5316f7ecb12e393e96dd7885c2751
Ariana <152944814+aam214@users.noreply.github.com>
2024-10-08 17:50:12 -0500

    Increased milliseconds for setTimeout.

--- index.js
+++ index.js
@@ -7,13 +7,12 @@ increaseNumber(countNow);
 function increaseNumber(countNow) {
 let currentNumber = +countNow.innerText;
 const dataCeil = countNow.getAttribute("data-ceil");
+//increase by 1% when the function gets called.
 const increaseIt = dataCeil / 100;
 currentNumber = Math.ceil(currentNumber + increaseIt);
-
-
 if(currentNumber < dataCeil) {
   countNow.innerText = currentNumber;
-  setTimeout(() => increaseNumber(countNow), 5);
+  setTimeout(() => increaseNumber(countNow), 20);
 } else {
 countNow.innerText = dataCeil;
 }
